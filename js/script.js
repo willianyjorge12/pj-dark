f41a258c924272f9259bdab76fabbb217c7e136d
willianyjorge12 <willianyjorge2004@gmail.com>
2024-11-18 21:22:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var body = document.querySelector("body")
+var btnDarkMode = document.querySelector("#dark-mode")
+
+btnDarkMode.addEventListener("click",darkmode)
+
+function darkmode(){
+    body.classList.toggle("dark-mode")
+    if (condition) {
+        
+    } else {
+        
+    }
+}
